752a482115d0d54bef8235f84627cac4753ba36a
odie <odie.miranda@gmail.com>
2011-06-20 18:51:53 +0800

    adding db local info

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'gobeyond_cromes';
+$db['default']['password'] = 'crms123';
+$db['default']['database'] = 'gobeyond_cromes';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
